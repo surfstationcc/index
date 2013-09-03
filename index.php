d247bc0ae624bf6531f19f80c488e9ee2e6daa35
surfstation <t@surfstation.cc>
2013-09-03 15:32:38 +0900

    rssnized

--- index.php
+++ index.php
@@ -162,6 +162,34 @@
 		}
 	}
 	</style>
+	<script src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+	<script>
+	$(document).ready(function(){
+		/**
+		 * Ajax通信メソッド
+		 * @param type      : HTTP通信の種類
+		 * @param url       : リクエスト送信先のURL
+		 * @param dataType  : データの種類
+		 */
+		$("#load").html("<img src='loader.gif' width='64' height='64' />");
+		$.ajax({
+			type: "POST", url: "rss.php", dataType: "html",
+			success: function(data, dataType){
+				//結果が0件の場合
+				if(data == null){
+					data = 'No data';
+				}
+				document.getElementById('act').innerHTML=data;
+			},
+			error: function(XMLHttpRequest, textStatus, errorThrown) {
+				alert('Error : ' + errorThrown);
+			},
+			complete: function(data) {
+				$("#load").empty();
+			}
+		});
+	});
+	</script>
 </head>
 <body>
 <div id="wrap">
@@ -169,7 +197,6 @@
 		<h1>Surf<span>station</span></h1>
 	</div>
 	<div id="content">
-		<!--<h2>Activities</h2>-->
 		<ul>
 			<li><a href="http://blog.surfstation.co/">Blog</a></li>
 			<li><a href="http://scrap.surfstation.co/">Tumblr</a></li>
@@ -179,74 +206,9 @@
 			<li><a href="https://github.com/surfstationcc">Github</a></li>
 		</ul>
 
-<?php
-//取得するRSSのURL
-$rss = array('http://blog.surfstation.co/feed', 'http://scrap.surfstation.co/rss', 'http://twittergoodrss.herokuapp.com/68356123416599783907/surfstationco', 'http://rss.stagram.tk/feed.php?id=143365&username=surfstation&rss');
-
-//2番目は1つのRSSから読み込む件数、3番目は実際に表示する件数
-rssReader($rss, 10, 10);
-
-function rssReader($rss, $read_count=0, $view_count=0) {
-	$html = '<h2>Activities</h2>';
-	$html .= '<ul class="act">';
-	$ardaysort = array();
-	$arday = array();
-	$arcontent = array();
-	$rsscount = 0;
-	foreach ($rss as $url) {
-		$count = 0;
-		$rssclass = '';
-		switch($rsscount){
-		case 0:
-			$rssclass = '<i class="icon-circle blog"></i><span>';
-			break;
-		case 1:
-			$rssclass = '<i class="icon-tumblr tumblr"></i><span>';
-			break;
-		case 2:
-			$rssclass = '<i class="icon-twitter twitter"></i><span>';
-			break;
-		case 3:
-			$rssclass = '<i class="icon-instagram instagram"></i><span>';
-			break;
-		case 4:
-			$rssclass = '<i class="icon-foursquare foursquare"></i><span>';
-			break;
-		}
-		foreach (simplexml_load_file($url)->channel->item as $item) {
-			$count++;
-			$day = date('Y.m.d', strtotime($item->pubDate)); //M jS, Y
-			$daysort = date('YmdHs', strtotime($item->pubDate)); // ソート用
-			if($rsscount == 2){
-				// Twitterはdescriptionを取得
-				$content = '<li>'.$rssclass.$day.'</span><a href="'.$item->link.'" title="'.htmlspecialchars(strip_tags($item->description)).'">'.mb_substr(strip_tags($item->description),2,20,'utf-8').'</a></li>';
-			} else {
-				$content = '<li>'.$rssclass.$day.'</span><a href="'.$item->link.'" title="'.htmlspecialchars($item->title).'">'.mb_substr($item->title,0,20,'utf-8').'</a></li>';
-			}
-			array_push($ardaysort, $daysort);
-			array_push($arday, $day);
-			array_push($arcontent, $content);
-			if ($count >= $read_count) {
-				break;
-			}
-		}
-		$rsscount++;
-	}
-	// 日付の新しい順に並び替え
-	array_multisort($ardaysort, SORT_DESC, $arday, $arcontent);
-	$valcnt = 0;
-	foreach ($arcontent as $value) {
-		$valcnt++;
-		$html .= $value;
-		if ($valcnt >= $view_count) {
-			break;
-		}
-	}
-
-	$html .= '</ul>';
-	echo $html;
-}
-?>
+		<h2>Activities</h2>
+		<div id="load"></div>
+		<ul id="act" class="act"></ul>
 
 		<h2>About</h2>
 		<dl class="about clearfix">
